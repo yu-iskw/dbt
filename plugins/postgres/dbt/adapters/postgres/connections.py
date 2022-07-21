999ed0b74ccb0e170f2b58029fdbfe3fc7ac83fd
Nathaniel May <nathaniel.may@fishtownanalytics.com>
2022-07-21 13:10:52 -0400

    postgres: add exponential backoff to connection retries (#5503)
    add exponential backoff to postgres connection retries

--- plugins/postgres/dbt/adapters/postgres/connections.py
+++ plugins/postgres/dbt/adapters/postgres/connections.py
@@ -145,11 +145,15 @@ class PostgresConnectionManager(SQLConnectionManager):
             psycopg2.errors.OperationalError,
         ]
 
+        def exponential_backoff(attempt: int):
+            return attempt * attempt
+
         return cls.retry_connection(
             connection,
             connect=connect,
             logger=logger,
             retry_limit=credentials.retries,
+            retry_timeout=exponential_backoff,
             retryable_exceptions=retryable_exceptions,
         )
 
