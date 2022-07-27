c3891d78e4d8e0b8de7e7c1a18516fb25a48b7df
Vyacheslav <2552891@gmail.com>
2022-07-27 18:40:32 +0200

    [CT-700] [Bug] Logging tons of asterisks for sensitive env vars  (#5518)
    * [CT-700] [Bug] Logging tons of asterisks when sensitive env vars are missing
    
    * [CT-700][Bug] Added changelog entry
    
    * Updated the changelog body message

--- core/dbt/events/functions.py
+++ core/dbt/events/functions.py
@@ -116,7 +116,7 @@ def stop_capture_stdout_logs() -> None:
 
 
 def env_secrets() -> List[str]:
-    return [v for k, v in os.environ.items() if k.startswith(SECRET_ENV_PREFIX)]
+    return [v for k, v in os.environ.items() if k.startswith(SECRET_ENV_PREFIX) and v.strip()]
 
 
 def scrub_secrets(msg: str, secrets: List[str]) -> str:
