e5ac9df06906b4446b7fd74cb0bf5ca525f7e1d1
Léon Stefani <leon@brigad.co>
2022-08-09 16:59:58 +0200

    Fix postgres handling for unlimited varchars (#5292)
    * Fix postgres handling for unlimited varchars
    
    * fix: correctly name varchar
    
    * chore: added changelog entry
    
    * Update .changes/unreleased/Fixes-20220523-103843.yaml
    
    Co-authored-by: Emily Rockman <ebuschang@gmail.com>
    
    Co-authored-by: Emily Rockman <ebuschang@gmail.com>

--- plugins/postgres/dbt/adapters/postgres/relation.py
+++ plugins/postgres/dbt/adapters/postgres/relation.py
@@ -26,7 +26,9 @@ class PostgresRelation(BaseRelation):
 class PostgresColumn(Column):
     @property
     def data_type(self):
-        # on postgres, do not convert 'text' to 'varchar()'
-        if self.dtype.lower() == "text":
+        # on postgres, do not convert 'text' or 'varchar' to 'varchar()'
+        if self.dtype.lower() == "text" or (
+            self.dtype.lower() == "character varying" and self.char_size is None
+        ):
             return self.dtype
         return super().data_type
