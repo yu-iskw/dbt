7efb6ab62dae3ea9da4e6e28e9a30aa4e0e74919
ilanbenb <12747039+ilanbenb@users.noreply.github.com>
2022-08-12 20:07:30 +0300

    Incremental model show reason for on_schema_change fail failures (#5505)
    * show reason for schema change failures
    
    When the incremental model fails, I do not get the context I need to easily fix my discrepency.
    Adding more info
    
    * Update on_schema_change.sql
    
    Fix identation
    
    * Added changie changes
    
    Added changie changes
    
    * Update on_schema_change.sql
    
    Trim whitespaces
    
    * Update on_schema_change.sql
    
    Log message text  enhancement

--- core/dbt/include/global_project/macros/materializations/models/incremental/on_schema_change.sql
+++ core/dbt/include/global_project/macros/materializations/models/incremental/on_schema_change.sql
@@ -119,6 +119,11 @@
                 - set the `on_schema_change` config to either append_new_columns or sync_all_columns, depending on your situation.
                 - Re-run the incremental model with `full_refresh: True` to update the target schema.
                 - update the schema manually and re-run the process.
+
+              Additional troubleshooting context:
+                 Source columns not in target: {{ schema_changes_dict['source_not_in_target'] }}
+                 Target columns not in source: {{ schema_changes_dict['target_not_in_source'] }}
+                 New column types: {{ schema_changes_dict['new_target_types'] }}
           {% endset %}
 
           {% do exceptions.raise_compiler_error(fail_msg) %}
