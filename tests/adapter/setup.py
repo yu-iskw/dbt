d80de8231696b780123fe825436fc25be601d936
Doug Beatty <44704949+dbeatty10@users.noreply.github.com>
2022-08-18 06:27:30 -0600

    Update "Homepage" link for dbt-tests-adapter on PyPI (#5678)

--- tests/adapter/setup.py
+++ tests/adapter/setup.py
@@ -35,7 +35,7 @@ setup(
     long_description_content_type="text/markdown",
     author="dbt Labs",
     author_email="info@dbtlabs.com",
-    url="https://github.com/dbt-labs/dbt-tests-adapter",
+    url="https://github.com/dbt-labs/dbt-core/tree/main/tests/adapter",
     packages=find_namespace_packages(include=["dbt", "dbt.*"]),
     install_requires=[
         "dbt-core=={}".format(package_version),
