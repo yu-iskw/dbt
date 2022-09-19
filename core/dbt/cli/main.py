4c29d48d1c5af0f1432fcaa5429fe0cd33692fb9
Ian Knox <81931810+iknox-fa@users.noreply.github.com>
2022-09-19 12:36:38 -0500

    Flags work with Click (#5790)

--- core/dbt/cli/main.py
+++ core/dbt/cli/main.py
@@ -1,37 +1,52 @@
+import inspect  # This is temporary for RAT-ing
+import sys
+from copy import copy
+from pprint import pformat as pf  # This is temporary for RAT-ing
+
 import click
 from dbt.cli import params as p
-import sys
+from dbt.cli.flags import Flags
 
-# This is temporary for RAT-ing
-import inspect
-from pprint import pformat as pf
+
+def cli_runner():
+    # Alias "list" to "ls"
+    ls = copy(cli.commands["list"])
+    ls.hidden = True
+    cli.add_command(ls, "ls")
+
+    # Run the cli
+    cli()
 
 
 # dbt
 @click.group(
+    context_settings={"help_option_names": ["-h", "--help"]},
     invoke_without_command=True,
     no_args_is_help=True,
     epilog="Specify one of these sub-commands and you can find more help from there.",
 )
 @click.pass_context
-@p.version
+@p.anonymous_usage_stats
 @p.cache_selected_only
 @p.debug
+@p.enable_legacy_logger
+@p.event_buffer_size
 @p.fail_fast
+@p.log_cache_events
 @p.log_format
+@p.macro_debugging
 @p.partial_parse
 @p.print
 @p.printer_width
 @p.quiet
-@p.send_anonymous_usage_stats
+@p.record_timing_info
 @p.static_parser
 @p.use_colors
 @p.use_experimental_parser
+@p.version
 @p.version_check
 @p.warn_error
 @p.write_json
-@p.event_buffer_size
-@p.record_timing
 def cli(ctx, **kwargs):
     """An ELT tool for managing your SQL transformations and data models.
     For more documentation on these commands, visit: docs.getdbt.com
@@ -46,26 +61,43 @@ def cli(ctx, **kwargs):
 # dbt build
 @cli.command("build")
 @click.pass_context
+@p.defer
+@p.exclude
+@p.fail_fast
+@p.full_refresh
+@p.indirect_selection
+@p.log_path
+@p.models
+@p.profile
+@p.profiles_dir
+@p.project_dir
+@p.selector
+@p.show
+@p.state
+@p.store_failures
+@p.target
+@p.target_path
+@p.threads
+@p.vars
+@p.version_check
 def build(ctx, **kwargs):
     """Run all Seeds, Models, Snapshots, and tests in DAG order"""
-    click.echo(
-        f"`{inspect.stack()[0][3]}` called\n kwargs: {kwargs}\n ctx: {pf(ctx.parent.params)}"
-    )
+    flags = Flags()
+    click.echo(f"`{inspect.stack()[0][3]}` called\n flags: {flags}")
 
 
 # dbt clean
 @cli.command("clean")
 @click.pass_context
-@p.project_dir
-@p.profiles_dir
 @p.profile
+@p.profiles_dir
+@p.project_dir
 @p.target
 @p.vars
 def clean(ctx, **kwargs):
     """Delete all folders in the clean-targets list (usually the dbt_packages and target directories.)"""
-    click.echo(
-        f"`{inspect.stack()[0][3]}` called\n kwargs: {kwargs}\n ctx: {pf(ctx.parent.params)}"
-    )
+    flags = Flags()
+    click.echo(f"`{inspect.stack()[0][3]}` called\n flags: {flags}")
 
 
 # dbt docs
@@ -78,86 +110,82 @@ def docs(ctx, **kwargs):
 # dbt docs generate
 @docs.command("generate")
 @click.pass_context
-@p.version_check
-@p.project_dir
-@p.profiles_dir
-@p.profile
-@p.target
-@p.vars
 @p.compile_docs
 @p.defer
-@p.threads
-@p.target_path
+@p.exclude
 @p.log_path
 @p.models
-@p.exclude
+@p.profile
+@p.profiles_dir
+@p.project_dir
 @p.selector
 @p.state
+@p.target
+@p.target_path
+@p.threads
+@p.vars
+@p.version_check
 def docs_generate(ctx, **kwargs):
     """Generate the documentation website for your project"""
-    click.echo(
-        f"`{inspect.stack()[0][3]}` called\n kwargs: {kwargs}\n ctx: {pf(ctx.parent.parent.params)}"
-    )
+    flags = Flags()
+    click.echo(f"`{inspect.stack()[0][3]}` called\n flags: {flags}")
 
 
 # dbt docs serve
 @docs.command("serve")
 @click.pass_context
-@p.project_dir
-@p.profiles_dir
+@p.browser
+@p.port
 @p.profile
+@p.profiles_dir
+@p.project_dir
 @p.target
 @p.vars
-@p.port
-@p.browser
 def docs_serve(ctx, **kwargs):
     """Serve the documentation website for your project"""
-    click.echo(
-        f"`{inspect.stack()[0][3]}` called\n kwargs: {kwargs}\n ctx: {pf(ctx.parent.parent.params)}"
-    )
+    flags = Flags()
+    click.echo(f"`{inspect.stack()[0][3]}` called\n flags: {flags}")
 
 
 # dbt compile
 @cli.command("compile")
 @click.pass_context
-@p.version_check
-@p.project_dir
-@p.profiles_dir
-@p.profile
-@p.target
-@p.vars
-@p.parse_only
-@p.threads
-@p.target_path
+@p.defer
+@p.exclude
+@p.full_refresh
 @p.log_path
 @p.models
-@p.exclude
+@p.parse_only
+@p.profile
+@p.profiles_dir
+@p.project_dir
 @p.selector
 @p.state
-@p.defer
-@p.full_refresh
+@p.target
+@p.target_path
+@p.threads
+@p.vars
+@p.version_check
 def compile(ctx, **kwargs):
     """Generates executable SQL from source, model, test, and analysis files. Compiled SQL files are written to the target/ directory."""
-    click.echo(
-        f"`{inspect.stack()[0][3]}` called\n kwargs: {kwargs}\n ctx: {pf(ctx.parent.params)}"
-    )
+    flags = Flags()
+    click.echo(f"`{inspect.stack()[0][3]}` called\n flags: {flags}")
 
 
 # dbt debug
 @cli.command("debug")
 @click.pass_context
-@p.version_check
-@p.project_dir
-@p.profiles_dir
+@p.config_dir
 @p.profile
+@p.profiles_dir
+@p.project_dir
 @p.target
 @p.vars
-@p.config_dir
+@p.version_check
 def debug(ctx, **kwargs):
     """Show some helpful information about dbt for debugging. Not to be confused with the --debug option which increases verbosity."""
-    click.echo(
-        f"`{inspect.stack()[0][3]}` called\n kwargs: {kwargs}\n ctx: {pf(ctx.parent.params)}"
-    )
+    flags = Flags()
+    click.echo(f"`{inspect.stack()[0][3]}` called\n flags: {flags}")
 
 
 # dbt deps
@@ -170,9 +198,8 @@ def debug(ctx, **kwargs):
 @p.vars
 def deps(ctx, **kwargs):
     """Pull the most recent version of the dependencies listed in packages.yml"""
-    click.echo(
-        f"`{inspect.stack()[0][3]}` called\n kwargs: {kwargs}\n ctx: {pf(ctx.parent.params)}"
-    )
+    flags = Flags()
+    click.echo(f"`{inspect.stack()[0][3]}` called\n flags: {flags}")
 
 
 # dbt init
@@ -181,147 +208,139 @@ def deps(ctx, **kwargs):
 @p.profile
 @p.profiles_dir
 @p.project_dir
+@p.skip_profile_setup
 @p.target
 @p.vars
-@p.skip_profile_setup
 def init(ctx, **kwargs):
     """Initialize a new DBT project."""
-    click.echo(
-        f"`{inspect.stack()[0][3]}` called\n kwargs: {kwargs}\n ctx: {pf(ctx.parent.params)}"
-    )
+    flags = Flags()
+    click.echo(f"`{inspect.stack()[0][3]}` called\n flags: {flags}")
 
 
 # dbt list
-# dbt TODO: Figure out aliasing for ls (or just c/p?)
 @cli.command("list")
 @click.pass_context
+@p.exclude
+@p.indirect_selection
+@p.models
+@p.output
+@p.output_keys
 @p.profile
 @p.profiles_dir
 @p.project_dir
-@p.target
-@p.vars
-@p.output
-@p.ouptut_keys
 @p.resource_type
-@p.models
-@p.indirect_selection
-@p.exclude
 @p.selector
 @p.state
+@p.target
+@p.vars
 def list(ctx, **kwargs):
     """List the resources in your project"""
-    click.echo(
-        f"`{inspect.stack()[0][3]}` called\n kwargs: {kwargs}\n ctx: {pf(ctx.parent.params)}"
-    )
+    flags = Flags()
+    click.echo(f"`{inspect.stack()[0][3]}` called\n flags: {flags}")
 
 
 # dbt parse
 @cli.command("parse")
 @click.pass_context
+@p.compile_parse
+@p.log_path
 @p.profile
 @p.profiles_dir
 @p.project_dir
 @p.target
-@p.vars
-@p.write_manifest
-@p.compile_parse
-@p.threads
 @p.target_path
-@p.log_path
+@p.threads
+@p.vars
 @p.version_check
+@p.write_manifest
 def parse(ctx, **kwargs):
     """Parses the project and provides information on performance"""
-    click.echo(
-        f"`{inspect.stack()[0][3]}` called\n kwargs: {kwargs}\n ctx: {pf(ctx.parent.params)}"
-    )
+    flags = Flags()
+    click.echo(f"`{inspect.stack()[0][3]}` called\n flags: {flags}")
 
 
 # dbt run
 @cli.command("run")
 @click.pass_context
+@p.defer
+@p.exclude
 @p.fail_fast
-@p.version_check
+@p.full_refresh
+@p.log_path
+@p.models
 @p.profile
 @p.profiles_dir
 @p.project_dir
-@p.target
-@p.vars
-@p.log_path
-@p.target_path
-@p.threads
-@p.models
-@p.exclude
 @p.selector
 @p.state
-@p.defer
-@p.full_refresh
+@p.target
+@p.target_path
+@p.threads
+@p.vars
+@p.version_check
 def run(ctx, **kwargs):
     """Compile SQL and execute against the current target database."""
-    click.echo(
-        f"`{inspect.stack()[0][3]}` called\n kwargs: {kwargs}\n ctx: {pf(ctx.parent.params)}"
-    )
+    flags = Flags()
+    click.echo(f"`{inspect.stack()[0][3]}` called\n flags: {flags}")
 
 
 # dbt run operation
 @cli.command("run-operation")
 @click.pass_context
+@p.args
 @p.profile
 @p.profiles_dir
 @p.project_dir
 @p.target
 @p.vars
-@p.args
 def run_operation(ctx, **kwargs):
     """Run the named macro with any supplied arguments."""
-    click.echo(
-        f"`{inspect.stack()[0][3]}` called\n kwargs: {kwargs}\n ctx: {pf(ctx.parent.params)}"
-    )
+    flags = Flags()
+    click.echo(f"`{inspect.stack()[0][3]}` called\n flags: {flags}")
 
 
 # dbt seed
 @cli.command("seed")
 @click.pass_context
-@p.version_check
+@p.exclude
+@p.full_refresh
+@p.log_path
+@p.models
 @p.profile
 @p.profiles_dir
 @p.project_dir
+@p.selector
+@p.show
+@p.state
 @p.target
-@p.vars
-@p.full_refresh
-@p.log_path
 @p.target_path
 @p.threads
-@p.models
-@p.exclude
-@p.selector
-@p.state
-@p.show
+@p.vars
+@p.version_check
 def seed(ctx, **kwargs):
     """Load data from csv files into your data warehouse."""
-    click.echo(
-        f"`{inspect.stack()[0][3]}` called\n kwargs: {kwargs}\n ctx: {pf(ctx.parent.params)}"
-    )
+    flags = Flags()
+    click.echo(f"`{inspect.stack()[0][3]}` called\n flags: {flags}")
 
 
 # dbt snapshot
 @cli.command("snapshot")
 @click.pass_context
+@p.defer
+@p.exclude
+@p.models
 @p.profile
 @p.profiles_dir
 @p.project_dir
-@p.target
-@p.vars
-@p.threads
-@p.models
-@p.exclude
 @p.selector
 @p.state
-@p.defer
+@p.target
+@p.threads
+@p.vars
 def snapshot(ctx, **kwargs):
     """Execute snapshots defined in your project"""
-    click.echo(
-        f"`{inspect.stack()[0][3]}` called\n kwargs: {kwargs}\n ctx: {pf(ctx.parent.params)}"
-    )
+    flags = Flags()
+    click.echo(f"`{inspect.stack()[0][3]}` called\n flags: {flags}")
 
 
 # dbt source
@@ -334,51 +353,49 @@ def source(ctx, **kwargs):
 # dbt source freshness
 @source.command("freshness")
 @click.pass_context
+@p.exclude
+@p.models
+@p.output_path  # TODO: Is this ok to re-use?  We have three different output params, how much can we consolidate?
 @p.profile
 @p.profiles_dir
 @p.project_dir
-@p.target
-@p.vars
-@p.threads
-@p.models
-@p.exclude
 @p.selector
 @p.state
-@p.output_path  # TODO: Is this ok to re-use?  We have three different output params, how much can we consolidate?
+@p.target
+@p.threads
+@p.vars
 def freshness(ctx, **kwargs):
     """Snapshots the current freshness of the project's sources"""
-    click.echo(
-        f"`{inspect.stack()[0][3]}` called\n kwargs: {kwargs}\n ctx: {pf(ctx.parent.parent.params)}"
-    )
+    flags = Flags()
+    click.echo(f"`{inspect.stack()[0][3]}` called\n flags: {flags}")
 
 
 # dbt test
 @cli.command("test")
 @click.pass_context
+@p.defer
+@p.exclude
 @p.fail_fast
-@p.version_check
-@p.store_failures
+@p.indirect_selection
+@p.log_path
+@p.models
 @p.profile
 @p.profiles_dir
 @p.project_dir
-@p.target
-@p.vars
-@p.indirect_selection
-@p.log_path
-@p.target_path
-@p.threads
-@p.models
-@p.exclude
 @p.selector
 @p.state
-@p.defer
+@p.store_failures
+@p.target
+@p.target_path
+@p.threads
+@p.vars
+@p.version_check
 def test(ctx, **kwargs):
     """Runs tests on data in deployed models. Run this after `dbt run`"""
-    click.echo(
-        f"`{inspect.stack()[0][3]}` called\n kwargs: {kwargs}\n ctx: {pf(ctx.parent.params)}"
-    )
+    flags = Flags()
+    click.echo(f"`{inspect.stack()[0][3]}` called\n flags: {flags}")
 
 
 # Support running as a module
 if __name__ == "__main__":
-    cli()
+    cli_runner()
--- tests/unit/test_cli.py
+++ tests/unit/test_cli.py
@@ -1,25 +1,50 @@
-from dbt.cli.main import cli
+import ast
+from inspect import getsource
+
 import click
+from dbt.cli import params
+from dbt.cli.main import cli
 
 
 class TestCLI:
     def test_commands_have_docstrings(self):
         def run_test(commands):
-            for _, command in commands.items():
-                if type(command) is click.core.Command:
+            for command in commands.values():
+                if type(command) is click.Command:
                     assert command.__doc__ is not None
-                if type(command) is click.core.Group:
+                if type(command) is click.Group:
                     run_test(command.commands)
 
         run_test(cli.commands)
 
-    def test_params_have_help_texts(self):
-        def run_test(commands):
-            for _, command in commands.items():
-                if type(command) is click.core.Command:
-                    for param in command.params:
-                        assert param.help is not None
-                if type(command) is click.core.Group:
-                    run_test(command.commands)
+    # TODO:  This isn't the ideal way to test params as
+    # they will be tested as many times as they are used as decorators.
+    # This is inefficent (obvs)
+    def test_unhidden_params_have_help_texts(self):
+        def run_test(command):
+            for param in command.params:
+                if not param.hidden:
+                    assert param.help is not None
+            if type(command) is click.Group:
+                for command in command.commands.values():
+                    run_test(command)
 
-        run_test(cli.commands)
+        run_test(cli)
+
+    def test_param_names_match_envvars(self):
+        def run_test(command):
+            for param in command.params:
+                if param.envvar is not None:
+                    assert "DBT_" + param.name.upper() == param.envvar
+            if type(command) is click.Group:
+                for command in command.commands.values():
+                    run_test(command)
+
+        run_test(cli)
+
+    def test_params_are_alpha_sorted(self):
+        root_node = ast.parse(getsource(params))
+        param_var_names = [
+            node.targets[0].id for node in ast.walk(root_node) if isinstance(node, ast.Assign)
+        ]
+        assert param_var_names == sorted(param_var_names)
