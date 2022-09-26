38ada8a68e51eed2fb543aaa808deb0c56d7fd40
dave-connors-3 <73915542+dave-connors-3@users.noreply.github.com>
2022-09-26 13:49:12 -0500

    merge exclude columns for incremental models (#5457)
    * exlcude cols like in dbt_utils.star
    
    * dispatch macro
    
    * changelog entry
    
    Co-authored-by: Gerda Shank <gerda@dbtlabs.com>

--- core/dbt/include/global_project/macros/materializations/models/incremental/merge.sql
+++ core/dbt/include/global_project/macros/materializations/models/incremental/merge.sql
@@ -5,7 +5,9 @@
 {% macro default__get_merge_sql(target, source, unique_key, dest_columns, predicates) -%}
     {%- set predicates = [] if predicates is none else [] + predicates -%}
     {%- set dest_cols_csv = get_quoted_csv(dest_columns | map(attribute="name")) -%}
-    {%- set update_columns = config.get('merge_update_columns', default = dest_columns | map(attribute="quoted") | list) -%}
+    {%- set merge_update_columns = config.get('merge_update_columns') -%}
+    {%- set merge_exclude_columns = config.get('merge_exclude_columns') -%}
+    {%- set update_columns = get_merge_update_columns(merge_update_columns, merge_exclude_columns, dest_columns) -%}
     {%- set sql_header = config.get('sql_header', none) -%}
 
     {% if unique_key %}
