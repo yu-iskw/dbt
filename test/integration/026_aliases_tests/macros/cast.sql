4f2fef1ecee8989be273a3a64aa5b2b1afab8076
Matthew McKnight <91097623+McKnight-42@users.noreply.github.com>
2022-09-28 11:03:12 -0500

    [CT-1166] test_aliases conversion (#5884)
    * init pr for 026 test conversion
    
    * removing old test, got all tests setup, need to find best way to handle regex in new test and see what we would actually want to do to test check we didn't run anything against
    
    * changes to test_alias_dupe_thorews_exeption passing locally now
    
    * adding test cases for final test
    
    * following the create new shcema method tests are passing up for review for core code
    
    * noving alias test to adapter zone
    
    * adding Base Classes
    
    * changing ref to fixtures
    
    * add double check to test
    
    * minor change to alt schema name formation, removal of unneeded setup fixture
    
    * typo in model names
    
    * update to main
    
    * pull models/schemas/macros into a fixtures file

--- test/integration/026_aliases_tests/macros/cast.sql
+++
@@ -1,9 +0,0 @@
-
-
-{% macro string_literal(s) -%}
-  {{ adapter.dispatch('string_literal', macro_namespace='test')(s) }}
-{%- endmacro %}
-
-{% macro default__string_literal(s) %}
-    '{{ s }}'::text
-{% endmacro %}
