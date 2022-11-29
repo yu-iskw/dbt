b3440417ad1e38a40c488c3632a63f5d2e0c88f4
Stu Kilgore <stu.kilgore@dbtlabs.com>
2022-11-29 13:30:47 -0600

    Add GHA workflow to build CLI API docs (#6187)

--- core/dbt/docs/source/conf.py
+++ core/dbt/docs/source/conf.py
@@ -7,7 +7,7 @@ import typing as t
 # For the full list of built-in configuration values, see the documentation:
 # https://www.sphinx-doc.org/en/master/usage/configuration.html
 
-sys.path.insert(0, os.path.abspath("../.."))
+sys.path.insert(0, os.path.abspath("../../.."))
 sys.path.insert(0, os.path.abspath("./_ext"))
 
 # -- Project information -----------------------------------------------------
