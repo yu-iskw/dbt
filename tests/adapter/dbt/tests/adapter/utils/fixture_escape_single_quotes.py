dd4b47d8b1fee9d50a2b5a98f76a49428def8c47
Doug Beatty <44704949+dbeatty10@users.noreply.github.com>
2023-01-10 17:26:35 -0700

    Check length of escaped strings in the adapter test (#6567)
    * Check length of escaped strings in the adapter test
    
    * One column per line to improve readability

--- tests/adapter/dbt/tests/adapter/utils/fixture_escape_single_quotes.py
+++ tests/adapter/dbt/tests/adapter/utils/fixture_escape_single_quotes.py
@@ -1,15 +1,37 @@
 # escape_single_quotes
 
 models__test_escape_single_quotes_quote_sql = """
-select '{{ escape_single_quotes("they're") }}' as actual, 'they''re' as expected union all
-select '{{ escape_single_quotes("they are") }}' as actual, 'they are' as expected
+select
+  '{{ escape_single_quotes("they're") }}' as actual,
+  'they''re' as expected,
+  {{ length(string_literal(escape_single_quotes("they're"))) }} as actual_length,
+  7 as expected_length
+
+union all
+
+select
+  '{{ escape_single_quotes("they are") }}' as actual,
+  'they are' as expected,
+  {{ length(string_literal(escape_single_quotes("they are"))) }} as actual_length,
+  8 as expected_length
 """
 
 
 # The expected literal is 'they\'re'. The second backslash is to escape it from Python.
 models__test_escape_single_quotes_backslash_sql = """
-select '{{ escape_single_quotes("they're") }}' as actual, 'they\\'re' as expected union all
-select '{{ escape_single_quotes("they are") }}' as actual, 'they are' as expected
+select
+  '{{ escape_single_quotes("they're") }}' as actual,
+  'they\\'re' as expected,
+  {{ length(string_literal(escape_single_quotes("they're"))) }} as actual_length,
+  7 as expected_length
+
+union all
+
+select
+  '{{ escape_single_quotes("they are") }}' as actual,
+  'they are' as expected,
+  {{ length(string_literal(escape_single_quotes("they are"))) }} as actual_length,
+  8 as expected_length
 """
 
 
@@ -21,4 +43,7 @@ models:
       - assert_equal:
           actual: actual
           expected: expected
+      - assert_equal:
+          actual: actual_length
+          expected: expected_length
 """
