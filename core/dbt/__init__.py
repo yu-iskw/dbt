4a4b89606b3a6153353d80f8882e7ee3c3c6f0d3
Kshitij Aranke <kshitij.aranke@dbtlabs.com>
2023-02-13 12:06:28 -0800

    Hoist dbt.cli.main into dbt.main namespace (#6939)

--- core/dbt/__init__.py
+++ core/dbt/__init__.py
@@ -1,7 +1,9 @@
-# N.B.
-# This will add to the package’s __path__ all subdirectories of directories on sys.path named after the package which effectively combines both modules into a single namespace (dbt.adapters)
-# The matching statement is in plugins/postgres/dbt/__init__.py
+# N.B. This will add to the package’s __path__ all subdirectories of directories on sys.path named after the package
+# which effectively combines both modules into a single namespace (dbt.adapters) The matching statement is in
+# plugins/postgres/dbt/__init__.py
 
 from pkgutil import extend_path
 
 __path__ = extend_path(__path__, __name__)
+
+from .cli import main  # noqa: F401
