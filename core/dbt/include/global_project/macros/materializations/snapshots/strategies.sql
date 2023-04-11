b450a5754e240d3ea617c4847ae4e92e39cec071
Robbert Hofman <117672004+RobbertDM@users.noreply.github.com>
2023-04-11 23:49:36 +0200

    Snapshot strategies: newline for subquery (#6780)
    * Snapshot strategies: newline for subquery
    
    * add changie output
    
    * add test for snapshot ending in comment
    
    * remove import to be flake8 compliant
    
    * add seed import
    
    * add newlines for flake8 compliance
    
    * typo fix
    
    * Fixing up a test, adding a comment or two
    
    * removed un-needed test fixtures
    
    * removed even more un-needed fixtures, collapsed test to single class
    
    * removed errant breakpoint()
    
    * Fix a little typo
    
    ---------
    
    Co-authored-by: Ian Knox <ian.knox@dbtlabs.com>
    Co-authored-by: Mila Page <67295367+VersusFacit@users.noreply.github.com>

--- core/dbt/include/global_project/macros/materializations/snapshots/strategies.sql
+++ core/dbt/include/global_project/macros/materializations/snapshots/strategies.sql
@@ -105,7 +105,11 @@
     {% elif check_cols_config is iterable and (check_cols_config | length) > 0 %}
         {#-- query for proper casing/quoting, to support comparison below --#}
         {%- set select_check_cols_from_target -%}
-          select {{ check_cols_config | join(', ') }} from ({{ node['compiled_code'] }}) subq
+            {#-- N.B. The whitespace below is necessary to avoid edge case issue with comments --#}
+            {#-- See: https://github.com/dbt-labs/dbt-core/issues/6781 --#}
+            select {{ check_cols_config | join(', ') }} from (
+                {{ node['compiled_code'] }}
+            ) subq
         {%- endset -%}
         {% set query_columns = get_columns_in_query(select_check_cols_from_target) %}
 
