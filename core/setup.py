d34c511fa59cd39380ad97886c394480ec347a0a
Gerda Shank <gerda@dbtlabs.com>
2023-05-09 13:10:09 -0400

    CT 2552 pin protobuf to >=4.0.0 (#7566)
    * Pin protobuf to >=4.0.0
    
    * Changie

--- core/setup.py
+++ core/setup.py
@@ -63,7 +63,7 @@ setup(
         "typing-extensions>=3.7.4",
         "werkzeug>=1,<3",
         "pathspec>=0.9,<0.12",
-        "protobuf>=3.18.3",
+        "protobuf>=4.0.0",
         "pytz>=2015.7",
         # the following are all to match snowflake-connector-python
         "requests<3.0.0",
