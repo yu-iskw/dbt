6d8b6459ebf038e1ecc536b193050683a6e5c9cd
Michelle Ark <MichelleArk@users.noreply.github.com>
2023-07-17 18:10:29 -0400

    bumpversion 1.7.0a1 (#8111)

--- plugins/postgres/setup.py
+++ plugins/postgres/setup.py
@@ -41,7 +41,7 @@ def _dbt_psycopg2_name():
 
 
 package_name = "dbt-postgres"
-package_version = "1.6.0b8"
+package_version = "1.7.0a1"
 description = """The postgres adapter plugin for dbt (data build tool)"""
 
 this_directory = os.path.abspath(os.path.dirname(__file__))
--- tests/adapter/dbt/tests/adapter/__version__.py
+++ tests/adapter/dbt/tests/adapter/__version__.py
@@ -1,1 +1,1 @@
-version = "1.6.0b8"
+version = "1.7.0a1"
