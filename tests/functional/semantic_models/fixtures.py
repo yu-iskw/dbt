7872f6a67087b08e377cb580248a70355c6c3c76
Quigley Malcolm <QMalcolm@users.noreply.github.com>
2023-07-31 14:15:09 -0700

    Add tests for specifcally checking the population of `SemanticModel.depends_on` (#8226)

---
+++ tests/functional/semantic_models/fixtures.py
@@ -0,0 +1,52 @@
+metricflow_time_spine_sql = """
+SELECT to_date('02/20/2023, 'mm/dd/yyyy') as date_day
+"""
+
+models_people_sql = """
+select 1 as id, 'Drew' as first_name, 'Banin' as last_name, 'yellow' as favorite_color, true as loves_dbt, 5 as tenure, current_timestamp as created_at
+union all
+select 2 as id, 'Jeremy' as first_name, 'Cohen' as last_name, 'indigo' as favorite_color, true as loves_dbt, 4 as tenure, current_timestamp as created_at
+union all
+select 3 as id, 'Callum' as first_name, 'McCann' as last_name, 'emerald' as favorite_color, true as loves_dbt, 0 as tenure, current_timestamp as created_at
+"""
+
+models_people_metrics_yml = """
+version: 2
+
+metrics:
+  - name: number_of_people
+    label: "Number of people"
+    description: Total count of people
+    type: simple
+    type_params:
+      measure: people
+    meta:
+        my_meta: 'testing'
+"""
+
+semantic_model_people_yml = """
+version: 2
+
+semantic_models:
+  - name: semantic_people
+    model: ref('people')
+    dimensions:
+      - name: favorite_color
+        type: categorical
+      - name: created_at
+        type: TIME
+        type_params:
+          time_granularity: day
+    measures:
+      - name: years_tenure
+        agg: SUM
+        expr: tenure
+      - name: people
+        agg: count
+        expr: id
+    entities:
+      - name: id
+        type: primary
+    defaults:
+      agg_time_dimension: created_at
+"""
---
+++ tests/functional/semantic_models/test_semantic_models.py
@@ -0,0 +1,52 @@
+import pytest
+
+from dbt.contracts.graph.manifest import Manifest
+from dbt.exceptions import CompilationError
+from dbt.tests.util import run_dbt
+
+
+from tests.functional.semantic_models.fixtures import (
+    models_people_sql,
+    metricflow_time_spine_sql,
+    semantic_model_people_yml,
+    models_people_metrics_yml,
+)
+
+
+class TestSemanticModelDependsOn:
+    @pytest.fixture(scope="class")
+    def models(self):
+        return {
+            "people.sql": models_people_sql,
+            "metricflow_time_spine.sql": metricflow_time_spine_sql,
+            "semantic_models.yml": semantic_model_people_yml,
+            "people_metrics.yml": models_people_metrics_yml,
+        }
+
+    def test_depends_on(self, project):
+        manifest = run_dbt(["parse"])
+        assert isinstance(manifest, Manifest)
+
+        expected_depends_on_for_people_semantic_model = ["model.test.people"]
+
+        number_of_people_metric = manifest.semantic_models["semantic_model.test.semantic_people"]
+        assert (
+            number_of_people_metric.depends_on.nodes
+            == expected_depends_on_for_people_semantic_model
+        )
+
+
+class TestSemanticModelUnknownModel:
+    @pytest.fixture(scope="class")
+    def models(self):
+        return {
+            "not_people.sql": models_people_sql,
+            "metricflow_time_spine.sql": metricflow_time_spine_sql,
+            "semantic_models.yml": semantic_model_people_yml,
+            "people_metrics.yml": models_people_metrics_yml,
+        }
+
+    def test_unknown_model_raises_issue(self, project):
+        with pytest.raises(CompilationError) as excinfo:
+            run_dbt(["parse"])
+        assert "depends on a node named 'people' which was not found" in str(excinfo.value)
