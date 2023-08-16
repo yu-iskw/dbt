17cd145f09660e708660793be0fbb51a68d1a819
Peter Webb <peter.webb@dbtlabs.com>
2023-08-16 16:42:48 -0400

    Temporarily disable test. (#8434)

--- tests/functional/artifacts/test_run_results.py
+++ tests/functional/artifacts/test_run_results.py
@@ -2,7 +2,6 @@ from multiprocessing import Process
 from pathlib import Path
 import json
 import pytest
-import platform
 from dbt.tests.util import run_dbt
 
 good_model_sql = """
@@ -41,7 +40,9 @@ class TestRunResultsTimingFailure:
         assert len(results.results[0].timing) > 0
 
 
-@pytest.mark.skipif(platform.system() != "Darwin", reason="Fails on linux in github actions")
+# This test is failing due to the faulty assumptions that run_results.json would
+# be written multiple times. Temporarily disabling.
+@pytest.mark.skip()
 class TestRunResultsWritesFileOnSignal:
     @pytest.fixture(scope="class")
     def models(self):
