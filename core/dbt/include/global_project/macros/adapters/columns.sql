556fad50df02bd96a427de16d3682a601d15c803
Emily Rockman <emily.rockman@dbtlabs.com>
2023-09-27 08:00:12 -0500

    Model contracts: raise warning for numeric types without specified scale (#8721)
    * add warning when contracting fields don't have precision
    
    * rename files
    
    * changelog
    
    * move tests out of adapter zone
    
    * Update core/dbt/include/global_project/macros/adapters/columns.sql
    
    Co-authored-by: colin-rogers-dbt <111200756+colin-rogers-dbt@users.noreply.github.com>
    
    * Apply suggestions from code review
    
    ---------
    
    Co-authored-by: colin-rogers-dbt <111200756+colin-rogers-dbt@users.noreply.github.com>

--- core/dbt/include/global_project/macros/adapters/columns.sql
+++ core/dbt/include/global_project/macros/adapters/columns.sql
@@ -36,24 +36,29 @@
     limit 0
 {% endmacro %}
 
-
 {% macro get_empty_schema_sql(columns) -%}
   {{ return(adapter.dispatch('get_empty_schema_sql', 'dbt')(columns)) }}
 {% endmacro %}
 
 {% macro default__get_empty_schema_sql(columns) %}
     {%- set col_err = [] -%}
+    {%- set col_naked_numeric = [] -%}
     select
     {% for i in columns %}
       {%- set col = columns[i] -%}
       {%- if col['data_type'] is not defined -%}
-        {{ col_err.append(col['name']) }}
+        {%- do col_err.append(col['name']) -%}
+      {#-- If this column's type is just 'numeric' then it is missing precision/scale, raise a warning --#}
+      {%- elif col['data_type'].strip().lower() in ('numeric', 'decimal', 'number') -%}
+        {%- do col_naked_numeric.append(col['name']) -%}
       {%- endif -%}
       {% set col_name = adapter.quote(col['name']) if col.get('quote') else col['name'] %}
       cast(null as {{ col['data_type'] }}) as {{ col_name }}{{ ", " if not loop.last }}
     {%- endfor -%}
     {%- if (col_err | length) > 0 -%}
       {{ exceptions.column_type_missing(column_names=col_err) }}
+    {%- elif (col_naked_numeric | length) > 0 -%}
+      {{ exceptions.warn("Detected columns with numeric type and unspecified precision/scale, this can lead to unintended rounding: " ~ col_naked_numeric ~ "`") }}
     {%- endif -%}
 {% endmacro %}
 
