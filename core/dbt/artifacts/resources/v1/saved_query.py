6813007e2856d40a705f9929aa19c764802b1285
Chenyu Li <chenyu.li@dbtlabs.com>
2024-05-21 13:57:30 -0700

    add saved query resource type (#10168)
    * add saved query resource type
    
    * nits

--- core/dbt/artifacts/resources/v1/saved_query.py
+++ core/dbt/artifacts/resources/v1/saved_query.py
@@ -2,13 +2,14 @@ from __future__ import annotations
 
 import time
 from dataclasses import dataclass, field
-from typing import Any, Dict, List, Optional
+from typing import Any, Dict, List, Literal, Optional
 
 from dbt_semantic_interfaces.type_enums.export_destination_type import (
     ExportDestinationType,
 )
 
 from dbt.artifacts.resources.base import GraphResource
+from dbt.artifacts.resources.types import NodeType
 from dbt.artifacts.resources.v1.components import DependsOn, RefArgs
 from dbt.artifacts.resources.v1.semantic_layer_components import (
     SourceFileMetadata,
@@ -82,6 +83,7 @@ class SavedQueryMandatory(GraphResource):
 
 @dataclass
 class SavedQuery(SavedQueryMandatory):
+    resource_type: Literal[NodeType.SavedQuery]
     description: Optional[str] = None
     label: Optional[str] = None
     metadata: Optional[SourceFileMetadata] = None
