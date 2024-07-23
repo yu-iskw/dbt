cd90d4493c1f2ac4a84e43be57ec29dcd5c4d2ef
Michelle Ark <MichelleArk@users.noreply.github.com>
2024-07-23 17:55:30 -0400

    add predicate to EventCatcher test util (#10482)

--- tests/utils.py
+++ tests/utils.py
@@ -1,5 +1,5 @@
 from dataclasses import dataclass, field
-from typing import List
+from typing import Callable, List
 
 from dbt_common.events.base_types import BaseEvent, EventMsg
 
@@ -8,9 +8,10 @@ from dbt_common.events.base_types import BaseEvent, EventMsg
 class EventCatcher:
     event_to_catch: BaseEvent
     caught_events: List[EventMsg] = field(default_factory=list)
+    predicate: Callable[[EventMsg], bool] = lambda event: True
 
     def catch(self, event: EventMsg):
-        if event.info.name == self.event_to_catch.__name__:
+        if event.info.name == self.event_to_catch.__name__ and self.predicate(event):
             self.caught_events.append(event)
 
     def flush(self) -> None:
