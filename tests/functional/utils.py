139b9ac54f0a3515bd422284faf8d76969d51c43
Michelle Ark <MichelleArk@users.noreply.github.com>
2024-09-13 18:19:33 -0400

    delete extraneous EventCatcher (#10710)

--- tests/functional/utils.py
+++ tests/functional/utils.py
@@ -1,11 +1,8 @@
 import os
 from contextlib import contextmanager
-from dataclasses import dataclass, field
 from datetime import datetime
 from pathlib import Path
-from typing import List, Optional
-
-from dbt_common.events.base_types import BaseEvent, EventMsg
+from typing import Optional
 
 
 @contextmanager
@@ -20,16 +17,3 @@ def up_one(return_path: Optional[Path] = None):
 
 def is_aware(dt: datetime) -> bool:
     return dt.tzinfo is not None and dt.tzinfo.utcoffset(dt) is not None
-
-
-@dataclass
-class EventCatcher:
-    event_to_catch: BaseEvent
-    caught_events: List[EventMsg] = field(default_factory=list)
-
-    def catch(self, event: EventMsg):
-        if event.info.name == self.event_to_catch.__name__:
-            self.caught_events.append(event)
-
-    def flush(self) -> None:
-        self.caught_events = []
