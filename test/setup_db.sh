77d8e3262a1b8b54eed6386c9b9841b73b080366
Emily Rockman <emily.rockman@dbtlabs.com>
2025-03-04 12:41:23 -0600

    handle ubuntu 20.04 deprecation (#11354)
    * update ubuntu 20.04 to 24.04
    
    * updates to ubuntu-latest instead
    
    * try postgres update
    
    * Change owner of db creation script so postgres can run it.
    
    * Add sudos.
    
    * Add debug logging.
    
    * Set execute bit on scripts.
    
    * More debug logging.
    
    * try a service
    
    * clean up and split the integrations tests by os
    
    ---------
    
    Co-authored-by: Peter Allen Webb <peter.webb@dbtlabs.com>

--- test/setup_db.sh
+++ test/setup_db.sh
@@ -11,24 +11,6 @@ PGPORT="${PGPORT:-5432}"
 export PGPORT
 PGHOST="${PGHOST:-localhost}"
 
-function connect_circle() {
-	# try to handle circleci/docker oddness
-	let rc=1
-	while [[ $rc -eq 1 ]]; do
-		nc -z ${PGHOST} ${PGPORT}
-		let rc=$?
-	done
-	if [[ $rc -ne 0 ]]; then
-		echo "Fatal: Could not connect to $PGHOST"
-		exit 1
-	fi
-}
-
-# appveyor doesn't have 'nc', but it also doesn't have these issues
-if [[ -n $CIRCLECI ]]; then
-	connect_circle
-fi
-
 for i in {1..10}; do
 	if pg_isready -h "${PGHOST}" -p "${PGPORT}" -U "${PGUSER}" ; then
 		break
