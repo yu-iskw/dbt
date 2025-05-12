28839335490bb723c6c3b99de17fb399156aab15
Emily Rockman <emily.rockman@dbtlabs.com>
2025-05-12 14:55:52 -0500

    fix command test (#11605)
    * fix command test
    
    * remove parameter

--- tests/functional/list/test_commands.py
+++ tests/functional/list/test_commands.py
@@ -29,10 +29,11 @@ commands_to_skip = {
     "show",
     "snapshot",
     "freshness",
+    "serve",  # this was actually serving up docs and caused issues locally
 }
 
 # Commands to happy path test
-commands = [command.value for command in Command if command.value not in commands_to_skip]
+commands = [command.to_list() for command in Command if command.value not in commands_to_skip]
 
 
 class TestRunCommands:
@@ -49,13 +50,12 @@ class TestRunCommands:
 
         shutil.rmtree(f"{project_root}/snapshots")
 
-    @pytest.mark.parametrize("dbt_command", [(command,) for command in commands])
     def test_run_commmand(
         self,
         happy_path_project,
-        dbt_command,
     ):
-        run_dbt([dbt_command])
+        for command in commands:
+            run_dbt(command)
 
 
 """
