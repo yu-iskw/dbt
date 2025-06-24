cd5d4be7ab36c64000cd578a5271f95100d7867c
Michelle Ark <MichelleArk@users.noreply.github.com>
2025-06-24 09:58:34 -0400

    bump dbt-common to 1.25.0 to access WarnErrorOptionsV2 (#11762)

--- core/setup.py
+++ core/setup.py
@@ -72,7 +72,7 @@ setup(
         "dbt-extractor>=0.5.0,<=0.6",
         "dbt-semantic-interfaces>=0.8.3,<0.9",
         # Minor versions for these are expected to be backwards-compatible
-        "dbt-common>=1.22.0,<2.0",
+        "dbt-common>=1.25.0,<2.0",
         "dbt-adapters>=1.15.2,<2.0",
         "dbt-protos>=1.0.315,<2.0",
         "pydantic<3",
