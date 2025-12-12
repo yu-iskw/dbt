bef7928e22b0a10db7c93cb996c18e42ae3b2234
Michelle Ark <MichelleArk@users.noreply.github.com>
2025-12-12 10:26:06 -0500

    remove unused performance CI framework (#12278)

--- performance/projects/01_2000_simple_models/models/path_76/node_760.sql
+++
@@ -1,9 +0,0 @@
-select 1 as id
-union all
-select * from {{ ref('node_0') }}
-union all
-select * from {{ ref('node_73') }}
-union all
-select * from {{ ref('node_244') }}
-union all
-select * from {{ ref('node_746') }}
