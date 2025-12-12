71a6e531023291ae252682d4221995b030c5ac21
Michelle Ark <MichelleArk@users.noreply.github.com>
2025-12-12 17:01:18 -0500

    delete deprecated create_adapter_plugins script (#12280)

--- core/scripts/create_adapter_plugins.py
+++
@@ -1,5 +0,0 @@
-# The create_adapter_plugins script is being replaced by a new interactive cookiecutter scaffold
-# that can be found https://github.com/dbt-labs/dbt-database-adapter-scaffold
-print(
-    "This script has been deprecated, to create a new adapter please visit https://github.com/dbt-labs/dbt-database-adapter-scaffold"
-)
